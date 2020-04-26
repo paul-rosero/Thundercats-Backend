5e959f46074bf4e6d6a7ccbd70cb9559e5e230a5
Paul Rosero <paul_rosero@yahoo.com>
2020-04-25 22:46:05 -0400

    add association to be able to use fav_chars.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :characters
     has_many :favorites
-    has_many :fav_chars, through: :favorites
+    has_many :fav_chars, through: :favorites, source: :character
 
     has_secure_password
 end
