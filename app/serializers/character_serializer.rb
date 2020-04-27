d348c27abde8dbfc18f22f1878fbaa0268684675
Paul Rosero <paul_rosero@yahoo.com>
2020-04-27 18:33:46 -0400

    add associations to be able to see a better json data.

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,4 +1,5 @@
 class CharacterSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :gender, :weapons, :species, :overview, :favorite
+  attributes :name, :gender, :weapons, :species, :overview, :favorite, :afiliation
+  has_many :users, through: :favorites
 end
