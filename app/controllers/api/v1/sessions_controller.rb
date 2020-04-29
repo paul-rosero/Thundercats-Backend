9c19986b4deabadaf914cec3d2ccc85483512fac
Paul Rosero <paul_rosero@yahoo.com>
2020-04-29 17:21:08 -0400

    add authentication for username and password and give errors if any.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        binding.pry
+        @user = User.find_by(username: params[:session][:username])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: {error: "Invalid Username or Password"}
+        end
     end
     
 end
