5892a2f1d9ff49dd3891ff598ab0dfc2bceebfc7
Paul Rosero <paul_rosero@yahoo.com>
2020-05-04 14:56:33 -0400

    change association so it can use serializer instead.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username, :email, :created_at, :updated_at 
   has_many :favorites
-  has_many :characters, through: :favorites
+  has_many :characters, serializer: CharacterSerializer
 end
