cc125c1fd9bfb18bb5e960a0775153eaab33ba58
Paul Rosero <paul_rosero@yahoo.com>
2020-05-04 21:13:01 -0400

    comment out code i dont think i will need until i finish then delete it.

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -15,24 +15,24 @@ class Api::V1::CharactersController < ApplicationController
   end
 
   # POST /characters
-  def create
-    @character = Character.new(character_params)
+  # def create
+  #   @character = Character.new(character_params)
 
-    if @character.save
-      render json: @character, status: :created, location: @character
-    else
-      render json: @character.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @character.save
+  #     render json: @character, status: :created, location: @character
+  #   else
+  #     render json: @character.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /characters/1
-  def update
-    if @character.update(character_params)
-      render json: @character
-    else
-      render json: @character.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @character.update(character_params)
+  #     render json: @character
+  #   else
+  #     render json: @character.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /characters/1
   def destroy
@@ -46,7 +46,7 @@ class Api::V1::CharactersController < ApplicationController
     end
 
     # Only allow a trusted parameter "white list" through.
-    def character_params
-      params.require(:character).permit(:name, :gender, :weapons, :species, :overview, :favorite, :user_id, :afiliation_id)
-    end
+    # def character_params
+    #   params.require(:character).permit(:name, :gender, :weapons, :species, :overview, :favorite, :user_id, :afiliation_id)
+    # end
 end
