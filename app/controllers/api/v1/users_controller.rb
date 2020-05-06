d60f2b059d790a96db467f1ab9b69f2c17267244
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 03:25:49 -0400

    add : infront of user_id in line 21.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      session[user_id] = @user.id
+      session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :created
     else
       resp = {
