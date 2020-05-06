ab6a89532eb63dd2821bc3c7cb65f0726680f243
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 02:44:30 -0400

    change password_digest to password and add error message.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,11 +17,13 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create 
     @user = User.new(user_params)
-
     if @user.save
       render json: @user, status: :created, location: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
@@ -47,6 +49,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :username, :password_digest, :email)
+      params.require(:user).permit(:name, :username, :password, :email)
     end
 end
