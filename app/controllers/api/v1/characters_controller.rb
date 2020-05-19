fea91062edc92de0107a948b1ff0af801512e24f
Paul Rosero <paul_rosero@yahoo.com>
2020-05-19 14:52:40 -0400

    comment out code to be deleted later.

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -35,9 +35,9 @@ class Api::V1::CharactersController < ApplicationController
   # end
 
   # DELETE /characters/1
-  def destroy
-    @character.destroy
-  end
+  # def destroy
+  #   @character.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
